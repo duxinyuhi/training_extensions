5f27dabadb9999e77a51d9460bc7148e0dd44ca5
Vladislav Sovrasov <sovrasov.vlad@gmail.com>
2020-11-25 17:48:11 +0300

    Add trainer for classification

--- pytorch_toolkit/ote/ote/modules/trainers/__init__.py
+++ pytorch_toolkit/ote/ote/modules/trainers/__init__.py
@@ -1,9 +1,11 @@
 from .base import BaseTrainer
 from .mmaction import MMActionTrainer
 from .mmdetection import MMDetectionTrainer
+from .reid import ReidTrainer
 
 __all__ = [
     'BaseTrainer',
     'MMActionTrainer',
     'MMDetectionTrainer',
+    'ReidTrainer',
 ]
